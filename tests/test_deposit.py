e389552f1223f76f12dfa0595dc799d924e1b341
Gin <hoangphuc442266@gmail.com>
2024-10-31 09:08:31 +0700

    update

--- tests/test_deposit.py
+++ tests/test_deposit.py
@@ -16,6 +16,7 @@ def login_page(driver):
 def deposit_page(driver):
     return DepositPage(driver)
 
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("34840", "abc", {Keys.TAB}),])
 def test_TCDP1(deposit_page, login_page, account_no, deposit_amount, description):
@@ -30,6 +31,8 @@ def test_TCDP1(deposit_page, login_page, account_no, deposit_amount, description
     error_message = deposit_page.get_error_message()
     assert error_message["deposit_amount_error"] == "Characters are not allowed"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("abcd", "12@", "valid"),])
 def test_TCDP2(deposit_page, login_page, account_no, deposit_amount, description):
@@ -44,14 +47,18 @@ def test_TCDP2(deposit_page, login_page, account_no, deposit_amount, description
     error_message = deposit_page.get_error_message()
     assert error_message["account_no_error"] == "Characters are not allowed"
     assert error_message["deposit_amount_error"] == "Special characters are not allowed"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
-    ("310103", "500", "SENT TO PHUC"),])
+    ("139416", "30", "SENT TO PHUC"),])
 def test_TCDP3(deposit_page, login_page, account_no, deposit_amount, description):
     login_page.open()
     login_page.login("mngr599099", "zAqyhYr")
     deposit_page.open()
     deposit_page.make_deposit(account_no, deposit_amount, description)
     assert "Guru99 Bank Manager HomePage" in deposit_page.driver.title
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("abcd", {Keys.TAB}, {Keys.TAB}),])
 def test_TCDP4(deposit_page, login_page, account_no, deposit_amount, description):
@@ -67,6 +74,8 @@ def test_TCDP4(deposit_page, login_page, account_no, deposit_amount, description
     assert error_message["account_no_error"] == "Characters are not allowed"
     assert error_message["deposit_amount_error"] == "Amount field must not be blank"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ({Keys.TAB}, {Keys.TAB}, "valid"),])
 def test_TCDP5(deposit_page, login_page, account_no, deposit_amount, description):
@@ -81,6 +90,8 @@ def test_TCDP5(deposit_page, login_page, account_no, deposit_amount, description
     error_message = deposit_page.get_error_message()
     assert error_message["account_no_error"] == "Account Number must not be blank"
     assert error_message["deposit_amount_error"] == "Amount field must not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("34840", {Keys.TAB}, "valid"),])
 def test_TCDP6(deposit_page, login_page, account_no, deposit_amount, description):
@@ -94,6 +105,8 @@ def test_TCDP6(deposit_page, login_page, account_no, deposit_amount, description
     alert.accept()
     error_message = deposit_page.get_error_message()
     assert error_message["deposit_amount_error"] == "Amount field must not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ({Keys.TAB}, "abc", {Keys.TAB}),])
 def test_TCDP7(deposit_page, login_page, account_no, deposit_amount, description):
@@ -109,6 +122,8 @@ def test_TCDP7(deposit_page, login_page, account_no, deposit_amount, description
     assert error_message["account_no_error"] == "Account Number must not be blank"
     assert error_message["deposit_amount_error"] == "Characters are not allowed"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("123@", "12", {Keys.TAB}),])
 def test_TCDP8(deposit_page, login_page, account_no, deposit_amount, description):
@@ -123,6 +138,8 @@ def test_TCDP8(deposit_page, login_page, account_no, deposit_amount, description
     error_message = deposit_page.get_error_message()
     assert error_message["account_no_error"] == "Special characters are not allowed"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ({Keys.TAB}, "12", {Keys.TAB}),])
 def test_TCDP9(deposit_page, login_page, account_no, deposit_amount, description):
@@ -137,7 +154,3 @@ def test_TCDP9(deposit_page, login_page, account_no, deposit_amount, description
     error_message = deposit_page.get_error_message()
     assert error_message["account_no_error"] == "Account Number must not be blank"
     assert error_message["description_error"] == "Description can not be blank"
-
-
-
-
--- tests/test_withdrawal.py
+++ tests/test_withdrawal.py
@@ -16,6 +16,7 @@ def login_page(driver):
 def withdrawal_page(driver):
     return WithdrawalPage(driver)
 
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("34840", "abc", {Keys.TAB}),])
 def test_TCWD1(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -23,13 +24,16 @@ def test_TCWD1(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
     error_message = withdrawal_page.get_error_message()
     assert error_message["deposit_amount_error"] == "Characters are not allowed"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("abcd", "12@", "valid"),])
 def test_TCWD2(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -37,29 +41,36 @@ def test_TCWD2(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
     error_message = withdrawal_page.get_error_message()
     assert error_message["account_no_error"] == "Characters are not allowed"
     assert error_message["deposit_amount_error"] == "Special characters are not allowed"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
-    ("310103", "130000", "SENT TO PHUC"),])
+    ("139416", "30", "SENT TO PHUC"),])
 def test_TCWD3(withdrawal_page, login_page, account_no, deposit_amount, description):
     login_page.open()
     login_page.login("mngr599099", "zAqyhYr")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    assert "Guru99 Bank Manager HomePage" in withdrawal_page.driver.title
+    assert "Guru99 Bank Amount Withdrawal Page" in withdrawal_page.driver.title
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("abcd", {Keys.TAB}, {Keys.TAB}),])
 def test_TCWD4(withdrawal_page, login_page, account_no, deposit_amount, description):
     login_page.open()
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
+    print(account_no, deposit_amount, description)
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
@@ -67,6 +78,8 @@ def test_TCWD4(withdrawal_page, login_page, account_no, deposit_amount, descript
     assert error_message["account_no_error"] == "Characters are not allowed"
     assert error_message["deposit_amount_error"] == "Amount field must not be blank"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ({Keys.TAB}, {Keys.TAB}, "valid"),])
 def test_TCWD5(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -74,13 +87,16 @@ def test_TCWD5(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
     error_message = withdrawal_page.get_error_message()
     assert error_message["account_no_error"] == "Account Number must not be blank"
     assert error_message["deposit_amount_error"] == "Amount field must not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("34840", {Keys.TAB}, "valid"),])
 def test_TCWD6(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -88,12 +104,15 @@ def test_TCWD6(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
     error_message = withdrawal_page.get_error_message()
     assert error_message["deposit_amount_error"] == "Amount field must not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ({Keys.TAB}, "abc", {Keys.TAB}),])
 def test_TCWD7(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -101,7 +120,8 @@ def test_TCWD7(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
@@ -109,6 +129,8 @@ def test_TCWD7(withdrawal_page, login_page, account_no, deposit_amount, descript
     assert error_message["account_no_error"] == "Account Number must not be blank"
     assert error_message["deposit_amount_error"] == "Characters are not allowed"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ("123@", "130000", {Keys.TAB}),])
 def test_TCWD8(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -116,13 +138,16 @@ def test_TCWD8(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
     error_message = withdrawal_page.get_error_message()
     assert error_message["account_no_error"] == "Special characters are not allowed"
     assert error_message["description_error"] == "Description can not be blank"
+
+
 @pytest.mark.parametrize("account_no, deposit_amount, description", [
     ({Keys.TAB}, "130000", {Keys.TAB}),])
 def test_TCWD9(withdrawal_page, login_page, account_no, deposit_amount, description):
@@ -130,14 +155,11 @@ def test_TCWD9(withdrawal_page, login_page, account_no, deposit_amount, descript
     login_page.login("mngr599498", "mEdAmeg")
     withdrawal_page.open()
     withdrawal_page.make_withdrawal(account_no, deposit_amount, description)
-    alert = WebDriverWait(withdrawal_page.driver, 10).until(EC.alert_is_present())
+    alert = WebDriverWait(withdrawal_page.driver,
+                          10).until(EC.alert_is_present())
     error_message_alert = alert.text
     assert error_message_alert == "Please fill all fields"
     alert.accept()
     error_message = withdrawal_page.get_error_message()
     assert error_message["account_no_error"] == "Account Number must not be blank"
     assert error_message["description_error"] == "Description can not be blank"
-
-
-
-
